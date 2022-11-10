a2122b65650399fb38c5af9d86fa2d143dd43af6
RallyDaria <dariazblewska@gmail.com>
2022-11-10 19:56:09 +0100

    Zadanie wszystkie steps

--- src/test/java/Features/wikipedia.feature
+++ src/test/java/Features/wikipedia.feature
@@ -1,7 +1,7 @@
 Feature:Wyszukiwanie fraz
 
   Scenario Outline:
-    Given Uzytkownik jest na stronie "www.wikipedia.org"
+    Given Uzytkownik jest na stronie "https://www.wikipedia.org"
     When Uzytkownik wpisuje <fraza> w pole wyszukiwania
     And  Uzytkownik klika przycisk Szukaj
     Then <tekst> zostaje poprawnie wyszukana
@@ -9,5 +9,5 @@ Feature:Wyszukiwanie fraz
     Examples:
       | fraza | tekst |
       | bdd   | bdd   |
-      |       |       |
-      |       |       |
+      #|       |       |
+      #|       |       |
--- src/test/java/Steps/WikipediaSteps.java
+++ src/test/java/Steps/WikipediaSteps.java
@@ -1,7 +1,13 @@
 package Steps;
 
+import com.sun.source.tree.AssertTree;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,14 +19,30 @@ public class WikipediaSteps {
     public static void setUp(){
         driver = new ChromeDriver();
     }
+    @Given("Uzytkownik jest na stronie {string}")
+    public void uzytkownik_jest_na_stronie(String adresUrl) {
+        driver.navigate().to(adresUrl);
+    }
+    @When("^Uzytkownik wpisuje (.+) w pole wyszukiwania$")
+    public void uzytkownik_wpisuje_w_pole_wyszukiwania(String fraza) {
+
+        driver.findElement(By.name("search")).sendKeys(fraza);
+    }
+    @When("Uzytkownik klika przycisk Szukaj")
+    public void uzytkownik_klika_przycisk_szukaj() {
+        driver.findElement(By.tagName("button")).click();
+        }
 
 
-
-
+    @Then("^(.+) zostaje poprawnie wyszukana$")
+    public void zostaje_poprawnie_wyszukana(String tekst)  {
+        //Assert.assertTrue(tekst,driver.findElement(By.id("firstHeading")).getText().toLowerCase().contains(tekst));
+        Assert.assertTrue(driver.findElement(By.id("firstHeading")).getText().toLowerCase().startsWith(tekst));
+    }
 
     @AfterAll
     public static void tearDown(){
-        driver.close();
+       // driver.close();
     }
 
 }
